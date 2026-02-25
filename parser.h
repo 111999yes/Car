2da5b62248ff409eff00e30226d2c168a7bcbcb4
111999yes <eason1080253@gmail.com>
2026-02-25 19:40:48 +0800

    add parser.h

--- parser.h
+++ parser.h
@@ -1,1 +1,29 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <sstream>
+
+void ParseEdge(std::string input, std::vector<std::vector<int>>& graph){
+    size_t idx = input.find(':');
+    if(idx == std::string::npos){
+        throw std::invalid_argument("Error: Invalid input format");
+    }
+    std::string front = input.substr(0, idx);
+    std::string back = input.substr(idx + 1);
+
+    int from = std::stoi(front);
+    if(from >= graph.size())
+        throw std::invalid_argument("Error: Invalid start point of the edge");
+    
+    int start = 0;
+    while(true){
+        int pos = back.find(',', start);
+        if(pos == std::string::npos){
+            graph[from].push_back(std::stoi(back.substr(start)));
+            break;
+        }
+        graph[from].push_back(std::stoi(back.substr(start, pos - start)));
+        start = pos + 1;
+    }
+}
